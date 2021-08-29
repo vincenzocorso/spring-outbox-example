031032f4276951f2073341682a8a8bf0ea9f4dae
Vincenzo Corso <vincenzocorso99@gmail.com>
2021-08-29 15:18:35 +0200

    build(todos-service): add todos service subproject

---
+++ todos-service/src/main/java/it/vincenzocorso/todosservice/TodosServiceApplication.java
@@ -0,0 +1,13 @@
+package it.vincenzocorso.todosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosServiceApplication.class, args);
+	}
+
+}
